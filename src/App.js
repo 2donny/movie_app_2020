1d4c7f1ebc990311cbbd2719bea53049a1f78437
EdonJung <2donny@naver.com>
2020-07-15 12:53:08 +0900

    first_draft

--- src/App.js
+++ src/App.js
@@ -29,28 +29,30 @@ class App extends React.Component {
   render() {
     const { isLoading, movies} = this.state; //isLoading = true <-> ES6 문법.
     return ( 
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map(movie => ( 
-            <Movie 
-              key={movie.id}
-              id={movie.id}
-              year={movie.year}
-              title={movie.title}
-              summary={movie.summary}
-              poster={movie.medium_cover_image}
-              genres = {movie.genres}
-            />
-            ))}
-          </div>
-        )
-        } 
-      </section>
+      <div id="potato">
+        <section className="container">
+          {isLoading ? (
+            <div className="loader">
+              <span className="loader__text">Loading...</span>
+            </div>
+          ) : (
+            <div className="movies">
+              {movies.map(movie => ( 
+              <Movie 
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres = {movie.genres}
+              />
+              ))}
+            </div>
+          )
+          } 
+        </section>
+      </div>
     )
   }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -4,14 +4,14 @@ import "./movie.css"
 
 function Movie({year, title, summary, poster, genres}) {
     return (
-        <div className="movies">
+        <div className="movie">
             <img src={poster} alt={title} title={title}></img>
-            <div className="movies__data">
-                <h3 className="movies__title">{title}</h3>
-                <h5 className="movies__year">{year}</h5>
-                <ul className="genres">
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="movie__genre">
                     {genres.map((genre, index) => (
-                        <li key={index} className="genres__genre">{genre}</li>
+                        <li key={index} className="movie__genre genre">{genre}</li>
                     ))}
                 </ul>
                 <p className="moveis__summary">{summary}</p>
