c3d6394fea44e7a15f1a6b325384578f30546d95
EdonJung <2donny@naver.com>
2020-07-16 01:48:57 +0900

    responsive Web page

--- src/Movie.js
+++ src/Movie.js
@@ -14,7 +14,7 @@ function Movie({year, title, summary, poster, genres}) {
                         <li key={index} className="movie__genre genre">{genre}</li>
                     ))}
                 </ul>
-                <p className="moveis__summary">{summary}</p>
+                <p className="movie__summary">{summary.slice(0, 120)}....</p>
             </div>
         </div>
     )
